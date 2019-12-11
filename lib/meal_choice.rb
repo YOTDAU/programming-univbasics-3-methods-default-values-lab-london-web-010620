d0cfd50586f1b09f9269bb50413281e861b5f3d9
Simon Tucker <sjtucker28@gmail.com>
2019-12-11 16:28:10 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,5 +2,5 @@
 # For output purposes, use "puts" instead of "print" or "p"
 
 def meal_choice (broccolli, macaroni, protein = "meat")
-  puts "A plate of meat with #{broccolli} and #{macaroni}."
+  puts "A plate of #{protein} with #{broccolli} and #{macaroni}."
 end
